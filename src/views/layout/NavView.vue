7bc1bf7560c45f030f74c9e2404f752c56544f88
junhyuk <jnhk123@knou.ac.kr>
2022-05-09 22:55:34 +0900

    Update NavView.vue

--- src/views/layout/NavView.vue
+++ src/views/layout/NavView.vue
@@ -48,6 +48,10 @@ export default {
     ...mapActions('user', ['setToken']),
 
     logout(){
+      if(!confirm('로그아웃 하시겠습니까?')){
+        return false;
+      }
+      
       this.setToken('');
     }
   }
