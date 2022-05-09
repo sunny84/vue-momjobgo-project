5f9109907ec0d8880297b4f3a0ea1621b9736126
junhyuk <jnhk123@knou.ac.kr>
2022-05-09 22:41:47 +0900

    Update index.js

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -44,15 +44,3 @@ export default {
     },
 
 }
-
-function toYmdHms(dateObj){
-
-    const year = String(dateObj.getFullYear());
-    const month = String(dateObj.getMonth() + 1).padStart(2,0);
-    const date = String(dateObj.getDate()).padStart(2,0);
-    const hour = String(dateObj.getHours()).padStart(2,0);
-    const minute = String(dateObj.getMinutes()).padStart(2,0);
-    const second = String(dateObj.getSeconds()).padStart(2,0);
-
-    return `${year}-${month}-${date} ${hour}:${minute}:${second}`;
-}
